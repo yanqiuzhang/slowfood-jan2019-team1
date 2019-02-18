586974f08c13d69823d5058281ff07c806def394
oliverochman <oliver.ochman@hotmail.com>
2019-02-18 09:21:26 +0100

    Initial commit, cloned from boilerplate

---
+++ features/support/env.rb
@@ -0,0 +1,24 @@
+require 'cucumber/rails'
+ActionController::Base.allow_rescue = false
+begin
+  DatabaseCleaner.strategy = :transaction
+rescue NameError
+  raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+end
+
+Chromedriver.set_version '2.33'
+
+Capybara.register_driver :selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+      args: %w( headless disable-popup-blocking disable-infobars)
+  )
+
+  Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+  )
+end
+Capybara.javascript_driver = :selenium
+Cucumber::Rails::Database.javascript_strategy = :truncation
+
