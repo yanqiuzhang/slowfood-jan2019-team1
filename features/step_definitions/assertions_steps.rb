f345f058c7a7e08a8fedca0998e0901a06c0d236
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-18 16:56:14 +0100

    adds cartify and devise gems. sets up welcome controller, creation of feature file for restaurants menu, creates basic and assertions steps.rb files

---
+++ features/step_definitions/assertions_steps.rb
@@ -0,0 +1,11 @@
+Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a food {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see product {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
