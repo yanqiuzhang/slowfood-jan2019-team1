511eaac2b1be6631db644cd5a001bc8d2cfce6e9
Alecia <aleciabenjamin@gmail.com>
2019-02-19 16:09:03 +0100

    add factory bot to products exist feature

--- features/support/basic_steps.rb
+++ features/support/basic_steps.rb
@@ -1,7 +1,8 @@
 Given("the following products exist") do |table|
-    # table is a Cucumber::MultilineArgument::DataTable
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+    table.hashes.each do |product|
+        FactoryBot.create(:product, product)
+    end 
+end
   
   Given("I visit the landing page") do
     pending # Write code here that turns the phrase above into concrete actions
