8560bc670f8c507e97310ae2409c1d78446babf9
Alecia <aleciabenjamin@gmail.com>
2019-02-19 15:57:39 +0100

    Creates Product model and specs

---
+++ features/support/basic_steps.rb
@@ -0,0 +1,12 @@
+Given("the following products exist") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("I visit the landing page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
