becca85c628f141b70ba45ff6d05d2a2eb854f68
SnailCoder1 <boa.matule@gmail.com>
2019-02-20 10:16:11 +0100

    product controller generated

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  def :name
+  end
+
+  def :description
+  end
+
+  def :price
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'products/:name'
+  get 'products/:description'
+  get 'products/:price'
 	root controller: :welcome, action: :index
 end
