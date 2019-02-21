bb0a9a4af5ae539e155671423e7a15f53fbbac64
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-21 17:42:08 +0100

    adds notices to layout file

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -3,13 +3,9 @@ Given("I visit the landing page") do
 end
 
 When("I fill in {string} with {string}") do |field, content|
-    fill_in field, with: content  
+    fill_in field, with: content
 end
-  
+
 When("I click on {string}") do |element|
     click_on element
-end
-  
-Then("I should see {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
 end
