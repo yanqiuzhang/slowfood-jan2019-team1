773d8fe76ce268eb1c3b49c926662b2cf399d2e9
nevroje <joakim.berglind@gmail.com>
2019-02-21 17:32:22 +0100

    create user model and add devise

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'turbolinks', '~> 5'
 gem 'bootsnap', '>= 1.1.0', require: false
-
+gem 'devise'
 
 group :development, :test do
   gem 'pry-byebug'
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,14 +2,14 @@ Given("I visit the landing page") do
     visit root_path
 end
 
-When("I fill in {string} with {string}") do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+When("I fill in {string} with {string}") do |field, content|
+    fill_in field, with: content  
+end
   
-  When("I click on {string}") do |element|
+When("I click on {string}") do |element|
     click_on element
-  end
+end
   
-  Then("I should see {string}") do |string|
+Then("I should see {string}") do |string|
     pending # Write code here that turns the phrase above into concrete actions
-  end
+end
