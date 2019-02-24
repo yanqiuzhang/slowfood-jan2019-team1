a12f457ce148d194d3094ee7714164f960001e00
olivia <olivia@evolve.se>
2019-02-24 15:39:56 +0100

    add and setup products in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
+products = [
+  [ "Paddy's Irish Nachos", 129 , "Handmade potato chips topped with seasoned beef and beans", "Starter" ],
+  [ "Chesapeake Crab Dip", 145 , "Served with toasted french baguette", "Starter" ],
+  [ "Pot O Gold Fritters", 79 , "Paddy's crispy fried potato fritters made with cheese, bacon and chives", "Starter" ],
+  [ "Limerick Fish and Chips", 159 , "Fresh cod battered and fried with our own Guiness beer batter served along side our hand cut french fries and homemade coleslaw", "Main Course" ],
+  [ "Chicken and Chips", 129 , "Tender and fresh chicken tenderloins fried in a light flour coating, served crisp and hot with our hand cut french fries and homemade coleslaw", "Main Course" ],
+  [ "Paddy's Bangers and Mash", 249 , "Homemade Irish sausage made right here at Paddy's with our own blend of spices, served on a bed of mashed potatoes and a light brown gravy", "Main Course" ],
+  [ "Pineapple Upside Down Cake", 89 , "An Irish Classic", "Dessert" ],
+  [ "Baked Cheesecake", 30000 , "Tasty Tasty", "Lunch", "Dessert" ],
+  [ "Apple Pie", 30000 , "Heated and served with homemade vanilla ice cream", "Dessert" ],
+]
+
+products.each do |name, price, description, category|
+  Product.create( name: name, price: price, description: description, category: category)
+end
